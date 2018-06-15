71d43317de309f4e4bdea384d41843229f82df78
dumasr3265 <40274860+dumasr3265@users.noreply.github.com>
2018-06-15 09:35:13 -0700

    Add files via upload
    Second submission- Calculates distance traveled at a set speed and distance

---
+++ P2HW1_DistanceTraveled_DumasRyan.py
@@ -0,0 +1,40 @@
+# CTI-110 
+# P2HW1 - Distance Traveled  
+# Ryan Dumas
+# 6/13/2018
+
+
+
+#This is a program to calculate the distance traveled at a given speed
+
+
+print("Your vehicle is traveling at 70 miles per hour")
+
+
+speed = 70
+
+
+
+#get distanceAfter6
+
+distanceAfter6 = speed * 6
+
+print("If you have been driving for 6 hours the distance you will have traveled in miles is:",distanceAfter6)
+
+
+
+#get distanceAfter10
+
+distanceAfter10 = speed * 10
+
+print("If you have been driving for 10 hours the distance you will have traveled in miles is:",distanceAfter10)
+
+
+
+
+#get distanceAfter15
+
+distanceAfter15 = speed * 15
+
+print("If you have been driving for 15 hours the distance you will have traveled in miles is:",distanceAfter15)
+
